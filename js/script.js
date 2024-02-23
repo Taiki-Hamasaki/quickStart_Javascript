88f0961f394fa2315a41fa506456f15083704a5b
Taiki-Hamasaki <70670989630@CAMPUS.MACHADO.MCH>
2024-02-23 09:01:42 -0300

    Simple Backup

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@
  * adicionando em elementos já existentes da página 
  * como seus elementos filhos 
  * Feito com a função appendChild(node: Node)
+ * 
+ * 
+ * IMPORTANTE
+ * Variaveis não se declaram tipagem
+ * mas podemos declarar a forma que será "usada"
+ * var - Variavel de escopo global(pode ser usado em qualquer lugar)
+ * let - Variavel de escopo de bloco(pode ser usado em blocos, perdendo-as após o fim dele, Ex: variaveis criadas dentro de funções)
+ * const - Dado de valor constante
  */
 
 // Atribuindo um elemento html em um objeto html
@@ -26,12 +34,40 @@ function comecar() {
     // Todo objeto que se aponta para um elemento html pode ter suas
     // propriedades css por meio do atributo style
     btnComecar.style.display = 'none';
-
-    let btnVoltar = document.createElement('button'); // botão criado
+    
+    
+    var btnVoltar = document.createElement('button'); // botão criado
     btnVoltar.id = 'voltar';
     btnVoltar.classList += ' buttons'; // adicionando uma classe ao botão criado
     btnVoltar.appendChild(document.createTextNode('Voltar')); //Adicionando um texto ao botão
-
+    
     // Adicionando o botão na página(no caso, dentro do body)
     document.body.appendChild(btnVoltar);
+    
+    
+    
+    novosEventos();
+}
+
+function novosEventos() {
+    let div = document.createElement('div');
+    div.id = 'buttons-div';
+    div.classList += ' disp-flex';
+    let buttons = [5];
+    for(let i = 0;i<5;i++) {
+        buttons[i] = document.createElement('button');
+        buttons[i].classList += ' buttons';
+        buttons[i].appendChild(document.createTextNode(`Evento [${i+1}]`));
+        div.appendChild(buttons[i]);
+    }
+    document.body.appendChild(div);
+}
+
+btnVoltar.addEventListener('click', voltar()); // Outra maneira(e melhor) de adicionar um evento, escolhendo o evento e adicionando quantas funções que desejar
+function voltar() {
+    console.log('')
+    audio.pause();
+    audio.currentTime = 0;
+    btnComecar.style.display = 'block';
+    document.body.removeChild(document.body.firstChild);
 }
